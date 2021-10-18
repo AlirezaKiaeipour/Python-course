ee156c2568ba7831db7316c944ee05adb707836c
Kiaeipour <a.kiaipoor@gmail.com>
2021-10-18 18:52:22 +0330

    python

--- Assignment 19/Palam_Poloom_Plush.py
+++ Assignment 19/Palam_Poloom_Plush.py
@@ -7,36 +7,33 @@ for i in range(5):
     user = input("Please Enter ✋ or 🤚: ")
     computer1 = random.choice(hand)
     computer2 = random.choice(hand)
-    if user == "✋" and computer1 == "✋" and computer2 == "✋":
+    if user == computer1 == computer2:
         print(user,computer1,computer2)
         print("Game is draw")
     elif user == "🤚" and computer1 == "✋" and computer2 == "✋":
         user_score +=1
-        print("User: ",user_score," Computer 1: ",computer1_score," Computer 2: ",computer2_score)
+        print("User: ",user_score,"Computer1: ",computer1_score,"Computer2: ",computer2_score)
     elif user == "✋" and computer1 == "🤚" and computer2 == "🤚":
         user_score +=1
-        print("User: ",user_score," Computer 1: ",computer1_score," Computer 2: ",computer2_score)
+        print("User: ",user_score,"Computer1: ",computer1_score,"Computer2: ",computer2_score)
     elif user == "🤚" and computer1 == "✋" and computer2 == "🤚":
         computer1_score +=1
-        print("User: ",user_score," Computer 1: ",computer1_score," Computer 2: ",computer2_score)
+        print("User: ",user_score,"Computer1: ",computer1_score,"Computer2: ",computer2_score)
     elif user == "✋" and computer1 == "🤚" and computer2 == "✋":
         computer1_score +=1
-        print("User: ",user_score," Computer 1: ",computer1_score," Computer 2: ",computer2_score)
+        print("User: ",user_score,"Computer1: ",computer1_score,"Computer2: ",computer2_score)
     elif user == "✋" and computer1 == "✋" and computer2 == "🤚":
         computer2_score +=1
-        print("User: ",user_score," Computer 1: ",computer1_score," Computer 2: ",computer2_score)
+        print("User: ",user_score,"Computer1: ",computer1_score,"Computer2: ",computer2_score)
     elif user == "🤚" and computer1 == "🤚" and computer2 == "✋":
         computer2_score +=1
-        print("User: ",user_score," Computer 1: ",computer1_score," Computer 2: ",computer2_score)
-    elif user == "🤚" and computer1 == "🤚" and computer2 == "🤚":
-        print("Game is draw")
-        print("User: ",user_score," Computer 1: ",computer1_score," Computer 2: ",computer2_score)
+        print("User: ",user_score,"Computer1: ",computer1_score,"Computer2: ",computer2_score)
 
-if user_score > computer1_score and computer2_score:
-    print("Yes. You Win")
-elif computer1_score > user_score and computer2_score:
-    print("Computer 1 is Win")
-elif computer2_score > user_score and computer1_score:
-    print("Computer 2 is Win")
-else:
+if user_score > computer1_score and user_score> computer2_score:
+    print("You win")
+elif computer1_score > user_score and computer1_score>computer2_score:
+    print("Computer1 is Win")
+elif computer2_score > user_score and computer2_score>computer1_score:
+    print("Computer2 is Win")
+elif user_score == computer1_score or user_score == computer2_score or computer2_score==computer1_score:
     print("Game is Draw")
